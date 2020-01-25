1e61cc2412851647e79932aa1de0882e19d56924
omkadri <omkadri@hotmail.com>
2020-01-25 17:09:36 -0500

    DE SCRIPTING TEST - 3: Figured out how time works in Lua
    - Created a table with variables for time in hours(12 hour clock), minutes, and seconds as well as the full current time.
    - printed all time variables on screen.

--- Clock/main.lua
+++ Clock/main.lua
@@ -1,14 +1,19 @@
-function love.load()
+function love.load()--this function is like the Start() function in C#
 
 end
---this is for preliminary setup before setting up the game
 
-function love.update(dt)
 
+function love.update(dt)--this function is like the Update() function in C#
+  --creating table for clock attributes
+  currentTime = {}
+    currentTime.fullTime = os.date()
+    currentTime.hour = os.date("%I") --based on 12 hour clock
+    currentTime.minute = os.date("%M")
+    currentTime.second = os.date("%S")
 end
---this function is basically like the update function in C#
 
 function love.draw()
+    --clock outline
     love.graphics.setColor(1,1,1)
     love.graphics.circle("line", 100, 100, 100, 100)
     love.graphics.setColor(0,0,1)
@@ -16,6 +21,11 @@ function love.draw()
     love.graphics.setColor(1,0,0)
     love.graphics.line(100,100, 100,150)
 
-    love.graphics.print(os.date(),0,200)
+    --display clock data on-screen
+    love.graphics.print(currentTime.fullTime,0,200)
+    love.graphics.print(currentTime.hour,0,225)
+    love.graphics.print(currentTime.minute,0,250)
+    love.graphics.print(currentTime.second,0,275)
+
 end
 --this function is for drawing graphics on screen (at every frame)
