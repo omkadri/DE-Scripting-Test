a165569e55a7a14c263ca4a3c852bbe819893fea
omkadri <omkadri@hotmail.com>
2020-01-25 15:55:58 -0500

    TOP DOWN SHOOTER - 2: Added more sprites to table (background, bullet, zombie)
    - Drew all sprites accordingly in love.draw()

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -1,6 +1,10 @@
 function love.load()
   sprites = {}
   sprites.player = love.graphics.newImage('sprites/player.png')
+  sprites.bullet = love.graphics.newImage('sprites/bullet.png')
+  sprites.zombie = love.graphics.newImage('sprites/zombie.png')
+  sprites.background = love.graphics.newImage('sprites/background.png')
+
 end
 
 function love.update(dt)
@@ -8,5 +12,9 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.draw(sprites.background, 0, 0)
+  love.graphics.draw(sprites.bullet, 200, 100)
   love.graphics.draw(sprites.player, 100, 100)
+  love.graphics.draw(sprites.zombie, 300, 100)
+
 end
