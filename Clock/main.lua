ebe063e6819da741fcb612da18c9f4523ac9c729
omkadri <omkadri@hotmail.com>
2020-01-25 23:38:55 -0600

    DE SCRIPTING TEST - 5: Clock hands now function properly, thanks to SOH CAH TOA
    I should have payed attention in grade 10 math - never thought I would ever use this shit.
    
    - created 6 terminalEnd variables for the clock hands (HourX, HourY, MinuteX, MinuteY, SecondX, SecondY)
    
    - each terminalEnd uses SOH CAH TOA accordingly
    
    - in these calculations, i changed the size of clockRadius accordingly for each hand
    
    - the end results of these calculations are implemented as coordinates the lines in love.draw()
    
    Housekeeping
    - printed clockRadius and the terminalEnd variables on-screenfor debugging.
    - added comments and tabbed lines accordingly

--- Clock/main.lua
+++ Clock/main.lua
@@ -6,14 +6,15 @@ end
 
 
 function love.update(dt)--this function is like the Update() function in C#
-  --creating table for clock attributes
-  currentTime = {}
-    currentTime.fullTime = os.date()
-    currentTime.hour = os.date("%I") --based on 12 hour clock
-    currentTime.minute = os.date("%M")
-    currentTime.second = os.date("%S")
 
-    --This table contains the x and y coordinates for all 3 hands of the clock
+  --creating table for getting time data
+    currentTime = {}
+      currentTime.fullTime = os.date()
+      currentTime.hour = os.date("%I") --based on 12 hour clock
+      currentTime.minute = os.date("%M")
+      currentTime.second = os.date("%S")
+
+    --This table contains the x and y coordinates for all terminal ends of the 3 clock hands
     clockHandCoordinates = {}
       clockHandCoordinates.hourX = 0
       clockHandCoordinates.hourY = 0
@@ -21,13 +22,18 @@ function love.update(dt)--this function is like the Update() function in C#
       clockHandCoordinates.minuteY = 0
       clockHandCoordinates.secondX = 0
       clockHandCoordinates.secondY = 0
+      --the zero is arbritrary for initialization.
 
     --trigonometric functions
-    theta = 0
     function thetaCalculation (currentTimeParameter, totalTimescale)
-      theta = (currentTimeParameter / totalTimescale) * (2*math.pi)
-      return theta
-      --example - to find the theta angle for the clock's minute hand, we would call thetaCalculation (currentTime.minute, 60)
+      theta = (currentTimeParameter / totalTimescale) * (2*math.pi) * (-1) + math.pi
+
+      --multiplying by (-1) makes the hand tick in the right direction, and adding math.pi makes the hand start at the top
+
+      return theta --theta variable is local to this function
+
+        --example - to find the theta angle for the clock's minute hand, we would call thetaCalculation (currentTime.minute, 60)
+
     end
 
     -- thetaCalculation for hours, minutes, and seconds
@@ -35,11 +41,15 @@ function love.update(dt)--this function is like the Update() function in C#
     thetaAngleMinutes = thetaCalculation (currentTime.minute, 60)
     thetaAngleSeconds = thetaCalculation (currentTime.second, 60)
 
+    --This function calculates the coordinate data for the terminal ends.
+    terminalEndSecondX = (math.sin(thetaAngleSeconds)*(clockRadius*0.25) + clockOriginX)
+    terminalEndSecondY = (math.cos(thetaAngleSeconds)*(clockRadius*0.25) + clockOriginY)
 
-    terminalEndX = 0
+    terminalEndMinuteX = (math.sin(thetaAngleMinutes)*(clockRadius*0.75) + clockOriginX)
+    terminalEndMinuteY = (math.cos(thetaAngleMinutes)*(clockRadius*0.75) + clockOriginY)
 
-
-
+    terminalEndHourX = (math.sin(thetaAngleHours)*(clockRadius*0.5) + clockOriginX)
+    terminalEndHourY = (math.cos(thetaAngleHours)*(clockRadius*0.5) + clockOriginY)
 
 end
 
@@ -51,10 +61,13 @@ function love.draw()
 
     --Coordinating each of the hands
     love.graphics.setColor(0,0,1)
-    love.graphics.line(clockOriginX,clockOriginY, 150,150)--blue line
+    love.graphics.line(clockOriginX,clockOriginY, terminalEndSecondX,terminalEndSecondY)--blue line(seconds)
 
     love.graphics.setColor(1,0,0)
-    love.graphics.line(clockOriginX,clockOriginY, 100,150)--red line
+    love.graphics.line(clockOriginX,clockOriginY, terminalEndMinuteX,terminalEndMinuteY)--red line (minutes)
+
+    love.graphics.setColor(0,1,0)
+    love.graphics.line(clockOriginX,clockOriginY, terminalEndHourX,terminalEndHourY)--green line (hours)
 
     --display clock data on-screen
     love.graphics.print(currentTime.fullTime,0,200)
@@ -65,6 +78,11 @@ function love.draw()
     love.graphics.print(thetaAngleHours,300,100)
     love.graphics.print(thetaAngleMinutes,300,200)
     love.graphics.print(thetaAngleSeconds,300,300)
+    love.graphics.print(clockRadius,300,400)
+    love.graphics.print(terminalEndSecondX,300,500)
+    love.graphics.print(terminalEndSecondY,300,550)
+
+
 
 end
 --this function is for drawing graphics on screen (at every frame)
