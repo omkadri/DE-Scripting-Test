5e523b424e11dcda82861b2e85d1d86c0cbc6d8b
omkadri <omkadri@hotmail.com>
2020-01-26 04:31:01 -0600

    DE SCRIPTING TEST - 7: Code cleanup - refactored various chunks of code in different functions.
    - Creater SetClockTransform() which allows the clock to be called in any position at any size.
    
    - Refactored various code chunks to make the following functions: [GetCurrentTimeData(), GetThetaAngles(), GetTerminalEndCoordinates(), drawClock(), drawDebugger()]
    
    - Placed clockFont inside of drawClock(). It's size is now proportionate to clockRadius (but it still needs tweaking)
    
    - Placed all functions at the bottom of the script for organization. Added headers accordingly.
    
    - Added comments and tabbing where needed

--- Clock/main.lua
+++ Clock/main.lua
@@ -1,95 +1,111 @@
-function love.load()--this function is like the Start() function in C#
-  clockOriginX = 100
-  clockOriginY = 100
-  clockRadius = 100
-  clockFont = love.graphics.newFont(20)
+function love.load()
+	--initial clock transform data. This can be changed by using SetClockTansform()
+	clockOriginX = 100
+	clockOriginY = 100
+	clockRadius = 100
 end
 
-
-function love.update(dt)--this function is like the Update() function in C#
-
-  --creating table for getting time data
-    currentTime = {}
-      currentTime.fullTime = os.date()
-      currentTime.hour = os.date("%I") --based on 12 hour clock
-      currentTime.minute = os.date("%M")
-      currentTime.second = os.date("%S")
-
-    --This table contains the x and y coordinates for all terminal ends of the 3 clock hands
-    clockHandCoordinates = {}
-      clockHandCoordinates.hourX = 0
-      clockHandCoordinates.hourY = 0
-      clockHandCoordinates.minuteX = 0
-      clockHandCoordinates.minuteY = 0
-      clockHandCoordinates.secondX = 0
-      clockHandCoordinates.secondY = 0
-      --the zero is arbritrary for initialization.
-
-    --trigonometric functions
-    function thetaCalculation (currentTimeParameter, totalTimescale)
-      theta = (currentTimeParameter / totalTimescale) * (2*math.pi) * (-1) + math.pi
-
-      --multiplying by (-1) makes the hand tick in the right direction, and adding math.pi makes the hand start at the top
-
-      return theta --theta variable is local to this function
-
-        --example - to find the theta angle for the clock's minute hand, we would call thetaCalculation (currentTime.minute, 60)
-
-    end
-
-    -- thetaCalculation for hours, minutes, and seconds
-    thetaAngleHours = thetaCalculation (currentTime.hour, 12)
-    thetaAngleMinutes = thetaCalculation (currentTime.minute, 60)
-    thetaAngleSeconds = thetaCalculation (currentTime.second, 60)
-
-
-    --This function calculates the coordinate data for the terminal ends.
-    --Terminal End of second hand
-    terminalEndSecondX = (math.sin(thetaAngleSeconds)*(clockRadius*0.75) + clockOriginX)
-    terminalEndSecondY = (math.cos(thetaAngleSeconds)*(clockRadius*0.75) + clockOriginY)
-
-    --Terminal End of minute hand
-    terminalEndMinuteX = (math.sin(thetaAngleMinutes)*(clockRadius*0.60) + clockOriginX)
-    terminalEndMinuteY = (math.cos(thetaAngleMinutes)*(clockRadius*0.60) + clockOriginY)
-
-    --Terminal End of hour hand
-    terminalEndHourX = (math.sin(thetaAngleHours)*(clockRadius*0.3) + clockOriginX)
-    terminalEndHourY = (math.cos(thetaAngleHours)*(clockRadius*0.3) + clockOriginY)
-
+function love.update(dt)
+	--setting clock position and size data
+	SetClockTansform(200,200,100)--(xPos, yPos, Radius)
+	GetCurrentTimeData()
+	
+	--trigonometry
+	GetThetaAngles()
+	GetTerminalEndCoordinates()
 end
-
 
 function love.draw()
-    --clock outline and appearence
+	drawClock()
+    drawDebugger()
+end
+
+
+-- **FUNCTIONS**
+
+--initialization
+function GetCurrentTimeData()
+	--creating table for getting time data from OS
+    currentTime = {}
+		currentTime.fullTime = os.date()
+		currentTime.hour = os.date("%I") --based on 12 hour clock
+		currentTime.minute = os.date("%M")
+		currentTime.second = os.date("%S")
+end
+
+function SetClockTansform(x, y, size)
+	clockOriginX = x
+	clockOriginY = y
+	clockRadius = size
+end
+
+
+--trigonometric functions
+function thetaAngleCalculator (currentTimeParameter, totalTimescale)
+	
+	theta = (currentTimeParameter / totalTimescale) * (2*math.pi) * (-1) + math.pi
+    --multiplying by (-1) makes the hand tick in the right direction, and adding math.pi makes the hand start at the top
+
+	return theta
+    --example: to find the theta angle for the clock's minute hand, we would call thetaAngleCalculator (currentTime.minute, 60)
+end
+
+function GetThetaAngles()
+	thetaAngleForHours = thetaAngleCalculator (currentTime.hour, 12)
+    thetaAngleForMinutes = thetaAngleCalculator (currentTime.minute, 60)
+    thetaAngleForSeconds = thetaAngleCalculator (currentTime.second, 60)
+end
+
+function GetTerminalEndCoordinates()
+    --This function calculates the coordinate data for the terminal ends of each clock hand.
+
+    --Terminal End of hour hand
+    terminalEndHourX = (math.sin(thetaAngleForHours)*(clockRadius*0.3) + clockOriginX)
+    terminalEndHourY = (math.cos(thetaAngleForHours)*(clockRadius*0.3) + clockOriginY)
+
+    --Terminal End of minute hand
+    terminalEndMinuteX = (math.sin(thetaAngleForMinutes)*(clockRadius*0.60) + clockOriginX)
+    terminalEndMinuteY = (math.cos(thetaAngleForMinutes)*(clockRadius*0.60) + clockOriginY)
+	
+	--Terminal End of second hand
+    terminalEndSecondX = (math.sin(thetaAngleForSeconds)*(clockRadius*0.75) + clockOriginX)
+    terminalEndSecondY = (math.cos(thetaAngleForSeconds)*(clockRadius*0.75) + clockOriginY)
+end
+
+
+--drawing
+function drawClock()
+
+	clockFont = love.graphics.newFont(clockRadius / 4, center)
+
+	--clock outline and appearence
     love.graphics.setColor(0.5,0.5,0.5)
     love.graphics.circle("fill", clockOriginX, clockOriginY, clockRadius)
     love.graphics.setColor(1,1,1)
     love.graphics.circle("line", clockOriginX, clockOriginY, clockRadius)
     love.graphics.circle("line", clockOriginX, clockOriginY, clockRadius)
 
-    --clock numbers
+	--clock numbers
     love.graphics.setColor(0,0,0)
     love.graphics.setFont(clockFont)
     love.graphics.print("3", (clockOriginX+(clockRadius*0.75)), clockOriginY - 7)
-
     love.graphics.print("6", clockOriginX - 7, (clockOriginY+(clockRadius*0.75)))
-
-
     love.graphics.print("9", (clockOriginX-(clockRadius*0.9)), clockOriginY - 8 )
-
     love.graphics.print("12", clockOriginX - 11, (clockOriginY-(clockRadius*0.9)))
 
+
     --Coordinating each of the hands
+		--Seconds Hand
     love.graphics.setColor(0,0,1)
     love.graphics.line(clockOriginX,clockOriginY, terminalEndSecondX,terminalEndSecondY)--blue line(seconds)
     love.graphics.circle("fill", terminalEndSecondX, terminalEndSecondY, 3) --terminalEnd of Hour hand
-
-
+	
+		--Minute Hand
     love.graphics.setColor(1,0,0)
     love.graphics.line(clockOriginX,clockOriginY, terminalEndMinuteX,terminalEndMinuteY)--red line (minutes)
     love.graphics.circle("fill", terminalEndMinuteX, terminalEndMinuteY, 3) --terminalEnd of Hour hand
 
-
+		--Hour Hand
     love.graphics.setColor(0,1,0)
     love.graphics.line(clockOriginX,clockOriginY, terminalEndHourX,terminalEndHourY)--green line (hours)
     love.graphics.circle("fill", terminalEndHourX, terminalEndHourY, 3) --terminalEnd of Hour hand
@@ -97,15 +113,9 @@ function love.draw()
     --circle in center of clock
     love.graphics.setColor(0.1,0.1,0.1)
     love.graphics.circle("fill", clockOriginX, clockOriginY, 5)
-
-    --Call debugger
-    debugger()
-
 end
---this function is for drawing graphics on screen (at every frame)
 
---debugger
-function debugger()
+function drawDebugger()
   if love.keyboard.isDown("i") then
     debuggerX = 250
     debuggerY = 0
@@ -117,14 +127,15 @@ function debugger()
     love.graphics.print("Current Hour: "..currentTime.hour,debuggerX,debuggerY + 100)
     love.graphics.print("Current Minute: "..currentTime.minute,debuggerX,debuggerY + 150)
     love.graphics.print("Current Second: "..currentTime.second,debuggerX,debuggerY + 200)
-    love.graphics.print("Theta Angle For Hour Hand: "..thetaAngleHours,debuggerX,debuggerY + 250)
-    love.graphics.print("Theta Angle For Minute Hand: "..thetaAngleMinutes,debuggerX,debuggerY + 300)
-    love.graphics.print("Theta Angle For Second Hand: "..thetaAngleSeconds,debuggerX,debuggerY + 350)
+    love.graphics.print("Theta Angle For Hour Hand: "..thetaAngleForHours,debuggerX,debuggerY + 250)
+    love.graphics.print("Theta Angle For Minute Hand: "..thetaAngleForMinutes,debuggerX,debuggerY + 300)
+    love.graphics.print("Theta Angle For Second Hand: "..thetaAngleForSeconds,debuggerX,debuggerY + 350)
     love.graphics.print("Clock Radius: "..clockRadius,debuggerX,debuggerY + 400)
     love.graphics.print("Terminal End Coordinate for Hour: ("..terminalEndHourX..", "..terminalEndHourY..")",debuggerX,debuggerY + 450)
     love.graphics.print("Terminal End Coordinate for Minute: ("..terminalEndMinuteX..", "..terminalEndMinuteY..")",debuggerX,debuggerY + 500)
     love.graphics.print("Terminal End Coordinate for Second: ("..terminalEndSecondX..", "..terminalEndSecondY..")",debuggerX,debuggerY + 550)
-
-
   end
 end
+
+
+--***Written by Omar Kadri***
