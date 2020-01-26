4208849cd7e150a64a68a5e17e71df07bbfc43a7
omkadri <omkadri@hotmail.com>
2020-01-26 17:09:16 -0500

    TOP DOWN SHOOTER - 4: Made player face mouse coordinates
    - Created playerMouseAngleCalculator(). This function's return value is used as rotrational data whe running love.graphics.draw() for the player sprite.
    
    - Also created playerOffset values so the the player sprite origin point is at the center of the image, and not the top left.
    
    Housekeeping
    - added comments and tabbing where needed.

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -4,6 +4,10 @@ function love.load()
   sprites.bullet = love.graphics.newImage('sprites/bullet.png')
   sprites.zombie = love.graphics.newImage('sprites/zombie.png')
   sprites.background = love.graphics.newImage('sprites/background.png')
+  
+  --offsets
+  playerOffsetX = sprites.player:getWidth()/2
+  playerOffsetY = sprites.player:getHeight()/2
 
   player = {}
   player.x = 100
@@ -13,27 +17,32 @@ function love.load()
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("s") then
-    player.y = player.y + player.speed * dt
-  end
+	if love.keyboard.isDown("s") then
+		player.y = player.y + player.speed * dt
+	end
 
-  if love.keyboard.isDown("w") then
-    player.y = player.y - player.speed * dt
-  end
+	if love.keyboard.isDown("w") then
+		player.y = player.y - player.speed * dt
+	end
 
-  if love.keyboard.isDown("a") then
-    player.x = player.x - player.speed * dt
-  end
+	if love.keyboard.isDown("a") then
+		player.x = player.x - player.speed * dt
+	end
 
-  if love.keyboard.isDown("d") then
-    player.x = player.x + player.speed * dt
-  end
+	if love.keyboard.isDown("d") then
+		player.x = player.x + player.speed * dt
+	end
 end
 
 function love.draw()
-  love.graphics.draw(sprites.background, 0, 0)
-  love.graphics.draw(sprites.bullet, 200, 100)
-  love.graphics.draw(sprites.player, player.x, player.y)
-  love.graphics.draw(sprites.zombie, 300, 100)
+	love.graphics.draw(sprites.background, 0, 0)
+	love.graphics.draw(sprites.bullet, 200, 100)
+	love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngleCalculation(), nil, nil, playerOffsetX, playerOffsetY)--we use nil to ignore parameters we don't want to mess with
+	love.graphics.draw(sprites.zombie, 300, 100)
+end
 
+
+
+function playerMouseAngleCalculation()
+	return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
 end
