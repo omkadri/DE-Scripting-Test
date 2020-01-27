080863fb00242153a93b9aa1d81fb997227afac4
omkadri <omkadri@hotmail.com>
2020-01-27 17:50:16 -0500

    PLATFORMER - 3: Set up spawnable platforms
    - Set up spawnable platforms with physics implmented
    
    -  applied coordinate offsetting (line 22 and line 32)
    
    - used table.insert() to pass platforms into platformTracker()

--- Platformer/main.lua
+++ Platformer/main.lua
@@ -8,6 +8,10 @@ function love.load()
 		sprites.player_stand = love.graphics.newImage('sprites/player_stand.png')
 	
 	require ('player')
+	
+	platformTracker = {}
+	
+	spawnPlatform(50,400,300,30)
 end
 
 function love.update(dt)
@@ -15,5 +19,20 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.draw(sprites.player_stand, player.body:getX(), player.body:getY())
+	love.graphics.draw(sprites.player_stand, player.body:getX(), player.body:getY(), nil, nil, nil, sprites.player_stand:getWidth()/2, sprites.player_stand:getHeight()/2)
+	
+	for i, p in ipairs(platformTracker) do
+		love.graphics.rectangle("fill", p.body:getX(), p.body:getY(), p.width, p.height)
+	end
+end
+
+function spawnPlatform(x, y, width, height)
+	local platform = {}
+		platform.body = love.physics.newBody(myWorld, x, y, "static")
+		platform.shape = love.physics.newRectangleShape(width/2, height/2, width, height)
+		platform.fixture = love.physics.newFixture(platform.body, platform.shape)
+		platform.width = width
+		platform.height = height
+		
+		table.insert(platformTracker, platform)
 end
