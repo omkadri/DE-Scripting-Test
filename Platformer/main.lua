be15398c781d35e2d63514ddffab576d8db5a7ee
omkadri <omkadri@hotmail.com>
2020-01-27 07:45:21 -0500

    PLATFORMER - 1: Set up repository.
    - accessed player coordinated from player.lua in main.lua

---
+++ Platformer/main.lua
@@ -0,0 +1,15 @@
+function love.load()
+	sprites = {}
+		sprites.coin_sheet = love.graphics.newImage('sprites/coin_sheet.png')
+		sprites.player_jump = love.graphics.newImage('sprites/player_jump.png')
+		sprites.player_stand = love.graphics.newImage('sprites/player_stand.png')
+	
+	require ('player')
+end
+
+function love.update()
+end
+
+function love.draw()
+	love.graphics.draw(sprites.player_stand, player.x, player.y)
+end
