521036d2a7a870b846e524b0fc46cf215b54d8b3
omkadri <omkadri@hotmail.com>
2020-01-30 13:03:13 -0500

    PLATFORMER - 6.1 : Player no longer kirby jumps
    - applied save to player.lua

--- Platformer/player.lua
+++ Platformer/player.lua
@@ -4,11 +4,14 @@ player = {}
 	player.shape = love.physics.newRectangleShape(66, 92)--encompasses sprite area
 	player.fixture = love.physics.newFixture(player.body, player.shape)
 	player.speed = 200
+	player.grounded = false
 	
 	function playerUpdate(dt)
 		if love.keyboard.isDown ("right") then
 			player.body:setX(player.body:getX() + player.speed * dt)
 		end
+		
+		
 		
 		if love.keyboard.isDown ("left") then
 			player.body:setX(player.body:getX() - player.speed * dt)
